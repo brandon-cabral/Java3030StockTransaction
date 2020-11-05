254c608390dd5055e94336208e1caa22ffbd0f02
brandon-cabral <69974587+brandon-cabral@users.noreply.github.com>
2020-11-05 09:24:48 -0500

    v0.8

--- src/main/java/com/uml/cabral/servlet/StockSearchServlet.java
+++ src/main/java/com/uml/cabral/servlet/StockSearchServlet.java
@@ -10,6 +10,7 @@ import com.uml.cabral.util.Interval;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
+@WebServlet("/stocks")
 public class StockSearchServlet extends HttpServlet{
 
 
--- src/main/java/edu/uml/bcabral/stocktrader/xml/XMLDomainObject.java
+++
@@ -1,8 +0,0 @@
-package edu.uml.bcabral.stocktrader.xml;
-
-/**
- * A marker class the indicates the class was created from an XML instance
- *
- */
-public interface XMLDomainObject {
-}
